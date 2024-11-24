48a0b005f7b58cb395aa40ff6146aea915db733c
ah140797 <ah140797@gmail.com>
2024-11-24 12:49:55 +0100

    main script with argparse

--- main.py
+++ main.py
@@ -1,10 +1,107 @@
 """Main python file to run the pipeline"""
 
+import os
+import argparse
 
-def main():
+import torch
+import numpy as np
 
-    return
+import torch
+
+from transformers import DistilBertForMaskedLM, DistilBertConfig
+
+
+from huggingface_hub import login
+import wandb
+
+from src.utils import get_oscar_dataset, get_available_device
+
+from src.tokenization import (
+    train_tokenizer,
+    tokenize_dataset,
+)
+
+from src.training import add_arguments, create_mlm_trainer
+
+import warnings
+
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+
+HUGGINGFACE_TOKEN = "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"
+
+UNK_TOKEN = "[UNK]"
+SPL_TOKENS = ["[PAD]", "[CLS]", "[SEP]", "[MASK]"] + [UNK_TOKEN]
+MAX_LENGTH = 512
+
+
+def main(args):
+
+    torch.cuda.empty_cache()
+    device, n_gpu = get_available_device()
+    print(f"Using device: {device}\nWith {n_gpu} GPUs")
+
+    login(HUGGINGFACE_TOKEN)
+    wandb.login()
+    os.environ["WANDB_LOG_MODEL"] = "checkpoint"
+
+    model_folder = "models/"
+    tokenizer_folder = "tokenizers/"
+
+    for language in args.languages:
+        for tokenizer_name in args.tokenizer_types:
+            for vocab_size in args.vocab_sizes:
+                for training_size in args.training_sizes:
+
+                    tokenizer_file = f"{tokenizer_folder}/tokenizer_{tokenizer_name}_{vocab_size}_{training_size}.json"
+                    model_file = f"{model_folder}/model_{tokenizer_name}_{vocab_size}_{training_size}.json"
+
+                    dataset = get_oscar_dataset(language, training_size)
+
+                    print("=" * 50)
+                    print(f"Start Training with configuration:")
+                    print(f"Language: {language}")
+                    print(f"Tokenizer Type: {tokenizer_name}")
+                    print(f"Vocabulary Size: {vocab_size}")
+                    print(f"Training Size: {training_size}")
+                    print("=" * 50)
+
+                    tokenizer = train_tokenizer(
+                        dataset,
+                        tokenizer_name,
+                        vocab_size,
+                        UNK_TOKEN,
+                        SPL_TOKENS,
+                        tokenizer_file,
+                    )
+                    tokenized_dataset = tokenize_dataset(dataset, tokenizer, MAX_LENGTH)
+
+                    configuration = DistilBertConfig(vocab_size=vocab_size)
+                    model = DistilBertForMaskedLM(configuration)
+
+                    trainer = create_mlm_trainer(
+                        tokenizer,
+                        model,
+                        tokenized_dataset,
+                        model_file,
+                        args.batch_size,
+                        args.learning_rate,
+                        args.wandb_run_name,
+                        args.epochs,
+                    )
+
+                    trainer.train()
+
+                return
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    add_arguments(parser)
+    args = parser.parse_args()
+
+    print("Arguments passed to the script:")
+    for arg, value in vars(args).items():
+        print(f"{arg}: {value}")
+
+    main(args)
