c36a1b0ffa1d1f0535e49023af5723215f497afe
ah140797 <ah140797@gmail.com>
2024-11-24 12:49:27 +0100

    src scripts, utils, tokenization, and training

---
+++ src/training.py
@@ -0,0 +1,134 @@
+from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from transformers import PreTrainedTokenizerFast
+from transformers import DistilBertForMaskedLM
+from datasets import IterableDataset
+
+TOKENIZER_BPE = "BPE"
+TOKENIZER_WPC = "WordPiece"
+TOKENIZER_UNI = "Unigram"
+
+
+def add_arguments(parser):
+    parser.add_argument(
+        "-l",
+        "--languages",
+        type=str,
+        nargs="+",  # Allow multiple languages
+        choices=["es", "tr"],  # "es" for Spanish, "tr" for Turkish
+        default=["es"],  # Default to Spanish if no language is specified
+        help="Language codes for the dataset. Provide one or more languages (choices: 'es' for Spanish, 'tr' for Turkish). Defaults to ['es'].",
+    )
+    parser.add_argument(
+        "-t",
+        "--tokenizer-types",
+        type=str,
+        nargs="+",  # Allow multiple tokenizers
+        choices=[TOKENIZER_BPE, TOKENIZER_WPC, TOKENIZER_UNI],
+        default=[TOKENIZER_BPE],  # Default to BPE if no tokenizer is specified
+        help=f"Which tokenizers to train. Provide one or more options. Choices: {TOKENIZER_BPE}, {TOKENIZER_WPC}, {TOKENIZER_UNI}. Defaults to {TOKENIZER_BPE}.",
+    )
+    parser.add_argument(
+        "-vs",
+        "--vocab-sizes",
+        type=int,
+        nargs="+",
+        default=[1000],
+        help="Vocabulary sizes for the trained tokenizers. Provide one or more values, e.g., 1000 2000. Defaults to [1000].",
+    )
+    parser.add_argument(
+        "-ts",
+        "--training-sizes",
+        type=int,
+        nargs="+",
+        default=[1000],
+        help="Training sizes for the tokenizers. Provide one or more values, e.g., 1000 5000. Defaults to [1000].",
+    )
+    parser.add_argument(
+        "-e",
+        "--epochs",
+        type=int,
+        default=3,
+        help="Number of training epochs. Defaults to 3.",
+    )
+    parser.add_argument(
+        "-b",
+        "--batch-size",
+        type=int,
+        default=16,
+        help="Training batch size. Defaults to 16.",
+    )
+    parser.add_argument(
+        "-lr",
+        "--learning-rate",
+        type=float,
+        default=5e-5,
+        help="Learning rate for training. Defaults to 5e-5.",
+    )
+    parser.add_argument(
+        "-wandb",
+        "--wandb-run-name",
+        type=str,
+        default="tokenizer_run",
+        help="Run name for tracking WandB. Defaults to tokenizer_run",
+    )
+
+
+def create_mlm_trainer(
+    tokenizer: PreTrainedTokenizerFast,
+    model: DistilBertForMaskedLM,
+    tokenized_dataset: IterableDataset,
+    model_file: str,
+    batch_size: int,
+    learning_rate: float,
+    run_name: str,
+    train_epochs: int,
+):
+    """
+    Creates and configures a Trainer for masked language model training. Masks 15% of the tokens for MLM training.
+
+    Args:
+        tokenizer (PreTrainedTokenizerFast): The tokenizer used to process the data.
+        model (DistilBertForMaskedLM): The model to train.
+        tokenized_dataset (IterableDataset): The tokenized dataset for training.
+        model_file (str): The file path to save the model.
+        batch_size (int): The batch size for training.
+        learning_rate (float): The learning rate for training.
+        run_name (str): The name for the training run (for tracking).
+        train_epochs (int): The number of training epochs.
+
+    Returns:
+        Trainer: The configured Trainer instance.
+    """
+
+    data_collator = DataCollatorForLanguageModeling(
+        tokenizer=tokenizer, mlm=True, mlm_probability=0.15, return_tensors="pt"
+    )
+
+    training_args = TrainingArguments(
+        output_dir=f"./{model_file}",
+        overwrite_output_dir=True,
+        lr_scheduler_type="linear",
+        max_steps=1,
+        logging_dir="./logs",
+        save_strategy="steps",
+        logging_steps=1,
+        use_cpu=False,
+        report_to="wandb",
+        run_name=run_name,
+        per_device_train_batch_size=batch_size,
+        learning_rate=learning_rate,
+        num_train_epochs=train_epochs,
+        # load_best_model_at_end=True,
+        # eval_strategy="steps",
+        # eval_steps=1,
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=tokenized_dataset,
+        tokenizer=tokenizer,
+        data_collator=data_collator,
+    )
+
+    return trainer
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,60 @@
-#
+from datasets import load_dataset
+from datasets import IterableDataset
+import torch
+
+
+def get_oscar_dataset(language: str, training_size: int) -> IterableDataset:
+    """Loads the OSCAR dataset in streaming-mode (iteratabledataset) for a specified language and training size.
+
+    Args:
+        language (str): The language code for the desired language subset of the OSCAR corpus (e.g., 'en' for English, 'tr' for Turkish).
+        training_size (int): The number of samples to retrieve from the dataset.
+
+    Returns:
+        IterableDataset: An iteretable dataset object of the OSCAR corpus, for language and training size specified.
+    """
+    dataset = load_dataset(
+        "oscar-corpus/oscar",
+        language=language,
+        streaming=True,
+        split="train",  # optional, but the dataset only has a train split
+    )
+
+    dataset = dataset.take(training_size)
+
+    return dataset
+
+
+def dataset_text_iterator(dataset: IterableDataset):
+    """Yields the 'text' column from an iterable dataset.
+
+
+    Args:
+        dataset (IterableDataset): An iterable dataset where each is expected to be a dictionary with a 'text' field.
+
+    Yields:
+        str: The text content from each sample in the dataset.
+    """
+    for sample in dataset:
+        yield sample["text"]
+
+
+def get_available_device():
+    """
+    Returns the best available device for PyTorch computations.
+    - If CUDA (GPU) is available, it returns 'cuda'.
+    - If MPS (Apple GPU) is available, it returns 'mps'.
+    - Otherwise, it returns 'cpu'.
+    """
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+        gpu_count = torch.cuda.device_count()
+        return device, gpu_count
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+        # returning to correctly handle batch size.
+        return device, 1
+    else:
+        device = torch.device("cpu")
+        # returning to correctly handle batch size.
+        return device, 1
