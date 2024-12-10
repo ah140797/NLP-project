7ba768433354b5b40724dffee650e9800bc595c7
ah140797 <ah140797@gmail.com>
2024-12-10 21:18:00 +0100

    no prints

--- src/training.py
+++ src/training.py
@@ -160,10 +160,8 @@ def create_mlm_trainer(
             dict: A dictionary containing the perplexity metric.
         """
         nonlocal tokenizer
-        print(f"eval_pred", eval_pred)
         logits = torch.tensor(eval_pred.predictions)
         labels = torch.tensor(eval_pred.label_ids)
-        print(f"labels", labels)
 
         mask = labels != -100
         labels = labels[mask]
