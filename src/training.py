fe695316dbb084cb0824b0f0ca21e20dee82c1b7
ah140797 <ah140797@gmail.com>
2024-12-10 20:17:52 +0100

    hh

--- src/training.py
+++ src/training.py
@@ -163,9 +163,7 @@ def create_mlm_trainer(
         logits = torch.tensor(eval_pred.predictions)
         labels = torch.tensor(eval_pred.label_ids)
 
-        # Convert logits to probabilities
         mask = labels != -100
-
         labels = labels[mask]
         logits = logits[mask]
 
@@ -175,11 +173,7 @@ def create_mlm_trainer(
         perplexity = torch.exp(probs)
 
         total_no_tokens = labels.size(0)
-        print(labels)
-        print(total_no_tokens)
         total_chars = sum(len(tokenizer.decode([label])) for label in labels)
-        print(tokenizer.decode(labels[0]))
-        print(total_chars)
 
         bpc = ln(perplexity) / ln(2) * (total_no_tokens / total_chars)
         return {"perplexity": perplexity.item(), "bpc": bpc}
